95adc15e0dfa61c6bde181bae587ed75b0307a4d
lychien <yuchienava@gmail.com>
2025-04-16 15:19:15 +0800

    Add files via upload

---
+++ lab10/script.js
@@ -0,0 +1,22 @@
+var dataUrl = 'https://api.unsplash.com/photos?client_id=812193ef71ca946e361ed541979a0cfd91e9419a19235fd05f51ea14233f020a&per_page=30';
+
+function getimg() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', dataUrl, true);
+  xhr.send();
+
+  xhr.onload = function () {
+    var data = JSON.parse(this.responseText);
+    add_new_img(data);
+  }
+}
+
+function add_new_img(dataset) {
+  var gal = document.getElementById("gallery");
+  gal.innerHTML = ""; // 清空舊的圖片
+  dataset.forEach(function(item) {
+    var img = document.createElement("img");
+    img.setAttribute("src", item.urls.small);
+    gal.appendChild(img);
+  });
+}
