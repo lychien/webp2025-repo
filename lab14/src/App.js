30d8293b82b3205309f1629c4395be935c41bdca
lychien <yuchienava@gmail.com>
2025-05-14 15:49:41 +0800

    Add files via upload

---
+++ lab14/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Game from './Game'; // 確保路徑正確
+import './App.css'; // 您可能還有其他的 App.css 樣式
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
